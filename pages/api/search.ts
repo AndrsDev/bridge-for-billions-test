72ffe73257088d9e01b02c8a5964bca660ad399c
Andres Sanabria <andrs.dev@gmail.com>
2020-12-03 13:37:54 -0600

    added query parameters to api endpoint

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -5,7 +5,8 @@ const DISCOGS_CONSUMER_SECRET = process.env.DISCOGS_CONSUMER_SECRET;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await fetch(`https://api.discogs.com/database/search?=&key=${DISCOGS_CONSUMER_KEY}&secret=${DISCOGS_CONSUMER_SECRET}`);
+    const { page, size } = req.query;
+    const response = await fetch(`https://api.discogs.com/database/search?=&key=${DISCOGS_CONSUMER_KEY}&secret=${DISCOGS_CONSUMER_SECRET}&page=${page}&per_page=${size}`);
     const data = await response.json();
     res.statusCode = 200
     res.json(data.results);
